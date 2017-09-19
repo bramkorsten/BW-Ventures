deaa89397102ed9e50aacb380f8af344689fa68e
ruben00james <rubenvlassak@hotmail.com>
2017-09-19 15:05:51 +0200

    Created a simple onClick listener for the login button to go to the Main Activity.

--- BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/LoginActivity.java
+++ BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.hizmet.bluewhaleventures;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        buttonLogin = (Button) this.findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
