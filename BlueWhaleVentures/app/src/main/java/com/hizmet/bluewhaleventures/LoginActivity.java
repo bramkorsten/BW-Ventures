007702bda7ce40131e11883cf91c82e5f679ad37
ruben00james <rubenvlassak@hotmail.com>
2017-09-25 14:12:10 +0200

    Added the Firebase Realtime Database to the app + added log

--- BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/LoginActivity.java
+++ BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/LoginActivity.java
@@ -3,17 +3,23 @@ package com.hizmet.bluewhaleventures;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
     Button buttonLogin;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        Log.d(TAG, "onCreate: " + database.getApp().getName());
 
         buttonLogin = (Button) this.findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
