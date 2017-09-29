4dc42df40ff39dcaa5b0588515b58f9b8361c49d
Dylan van Heugten <d.heugten@gmail.com>
2017-09-29 11:17:24 +0200

    Added Login Screen

---
+++ iOS_BW-Ventures/iOS_BW-Ventures/LoginScreen.swift
@@ -0,0 +1,46 @@
+//
+//  LoginScreen.swift
+//  iOS_BW-Ventures
+//
+//  Created by Fhict on 25/09/2017.
+//  Copyright © 2017 Lycyic. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
