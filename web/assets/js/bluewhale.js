6cec18f0c8bf7ff45e7f6abe6a17810ad961daf4
Arne Reijntjes <arne123@live.nl>
2017-10-09 11:34:19 +0200

    Long forgotten push

--- web/assets/js/bluewhale.js
+++ web/assets/js/bluewhale.js
@@ -7,3 +7,103 @@ function expandForm() {
 
   setTimeout(function(){ document.getElementById("login-title").classList.toggle('class-gone'); }, 300);
 }
+
+function tryLogin() {
+  var inputUsername = document.getElementById('user-input').value;
+  var inputPassword = document.getElementById('user-password').value;
+
+    firebase.auth().signInWithEmailAndPassword(inputUsername, inputPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    if (errorCode) {
+      //alert(errorCode);
+      document.getElementById('login-wrap').classList.toggle('wrong-login');
+
+        setTimeout(function(){
+          document.getElementById('login-wrap').classList.toggle('wrong-login');
+          //document.getElementById('user-input').value="";
+          //document.getElementById('user-password').value="";
+        }, 500);
+
+    }
+
+
+    //console.log(errorMessage);
+    // ...
+  });
+
+
+}
+
+
+      firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        goodLogin();
+        getClass();
+        console.log(user);
+      }
+      });
+
+function logout() {
+  firebase.auth().signOut().then(function() {
+    location.reload();
+  }, function(error) {
+    return(0);
+  });
+}
+
+function getClass() {
+  var el = document.getElementById('login-background');
+  if (hasClass(el, 'form-filter') == false) {
+    document.getElementById("login-background").classList.toggle('form-filter');
+    document.getElementById("background-img").classList.toggle('add-filter');
+  }
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+
+
+
+function goodLogin() {
+    document.getElementById("login-form").classList.toggle('form-login-succes');
+    document.getElementById("form-logo").classList.toggle('form-logo-center');
+}
+
+
+function getTab(x) {
+  if (x == 'clients') {
+    document.getElementById('profile-title-active').className='title-border-left';
+    document.getElementById('menu-item-clients').className='menu-item menu-item-top menu-item-active';
+    document.getElementById('menu-item-experiments').className='menu-item';
+    document.getElementById('menu-item-people').className='menu-item';
+
+    document.getElementById('title-clients').className='title-mid-active';
+    document.getElementById('title-experiments').className='';
+    document.getElementById('title-people').className='';
+  }
+  if (x == 'experiments') {
+    document.getElementById('profile-title-active').className='';
+    document.getElementById('menu-item-clients').className='menu-item menu-item-top';
+    document.getElementById('menu-item-experiments').className='menu-item menu-item-active';
+    document.getElementById('menu-item-people').className='menu-item';
+
+    document.getElementById('title-clients').className='';
+    document.getElementById('title-experiments').className='title-mid-active';
+    document.getElementById('title-people').className='';
+  }
+  if (x == 'people') {
+    document.getElementById('profile-title-active').className='title-border-right';
+    document.getElementById('menu-item-clients').className='menu-item menu-item-top';
+    document.getElementById('menu-item-people').className='menu-item menu-item-active';
+    document.getElementById('menu-item-experiments').className='menu-item';
+
+    document.getElementById('title-clients').className='';
+    document.getElementById('title-experiments').className='';
+    document.getElementById('title-people').className='title-mid-active';
+  }
+}
