6509d5b5e0ce96e2dd0e0d34c97601a62a10e9a2
Dylan van Heugten <d.heugten@gmail.com>
2017-10-30 11:45:07 +0100

    New empty commit

--- iOS_BW-Ventures/iOS_BW-Ventures/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  iOS_BW-Ventures
-//
-//  Created by Fhict on 25/09/2017.
-//  Copyright © 2017 Lycyic. All rights reserved.
-//
-
-import Foundation
---
+++ iOS_BW-Ventures/ventures/ventures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ventures
+//
+//  Created by Fhict on 30/10/2017.
+//  Copyright © 2017 Restart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
