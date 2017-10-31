9991c1f7934bf541a1b484ef33beb18a70d96ade
Dylan van Heugten <d.heugten@gmail.com>
2017-10-31 13:08:58 +0100

    Added functioning login screen

--- iOS_BW-Ventures/ventures/ventures/ViewController.swift
+++ iOS_BW-Ventures/ventures/ventures/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tbEmailAddress: UITextField!
+    @IBOutlet weak var tbPassword: UITextField!
+    
+    var email = ""
+    var password = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func loginAction(_ sender: Any) {
+        email = tbEmailAddress.text!
+        password = tbPassword.text!
+        
+        if self.tbEmailAddress.text == "" || self.tbPassword.text == "" {
+            // Alert to tell the user that there was an error because they didn't fill anything in one or both textfields
+            let alertController = UIAlertController(title: "Error", message: "Please enter and email address and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
+                
+                if error == nil {
+                    // Print to console
+                    print("Login successful")
+                } else {
+                    // Tell user that there is an error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
 
