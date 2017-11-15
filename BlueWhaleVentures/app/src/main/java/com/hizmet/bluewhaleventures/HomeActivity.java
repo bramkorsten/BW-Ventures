ace29ebe1c10a9df59e5cc71b6024180f85dea17
ruben00james <rubenvlassak@hotmail.com>
2017-11-15 08:59:21 +0100

    Fixed crash when no questions present.

--- BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/HomeActivity.java
+++ BlueWhaleVentures/app/src/main/java/com/hizmet/bluewhaleventures/HomeActivity.java
@@ -1,17 +1,24 @@
 package com.hizmet.bluewhaleventures;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.hizmet.bluewhaleventures.fragments.ExperimentsFragment;
 import com.hizmet.bluewhaleventures.fragments.InformationFragment;
 import com.hizmet.bluewhaleventures.fragments.SettingsFragment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,6 +26,8 @@ public class HomeActivity extends AppCompatActivity {
     InformationFragment informationFragment;
     SettingsFragment settingsFragment;
     Fragment currentFragment;
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+    private String[] permissions = {android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     // Bottom navigation
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -68,7 +77,41 @@ public class HomeActivity extends AppCompatActivity {
     };
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(android.Manifest.permission.RECORD_AUDIO, PackageManager.PERMISSION_GRANTED);
+                perms.put(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                if (perms.get(android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // All Permissions Granted
+
+                } else {
+                    // Permission Denied
+                    Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int permission1 = PermissionChecker.checkSelfPermission(this, permissions[0]);
+        int permission2 = PermissionChecker.checkSelfPermission(this, permissions[1]);
+        if (permission1 == PermissionChecker.PERMISSION_GRANTED && permission2 == PermissionChecker.PERMISSION_GRANTED) {
+            // Good to go, permissions already granted
+        } else {
+            // Permission not already granted, request permissions
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
